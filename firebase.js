3518bbda4cab4df57987b628a5c60f6b81a8a5e5
olivaresagustina <olivares.financiera@gmail.com>
2026-04-18 14:20:47 -0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -3,12 +3,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 
 const firebaseConfig = {
     apiKey: "AIzaSyB0AUy_O2St0qpDDEwJVmtJKZczF9aXEPw",
-    authDomain: "tienda-ropa-cadfc.firebaseapp.com",
-    projectId: "tienda-ropa-cadfc",
-    storageBucket: "tienda-ropa-cadfc.firebasestorage.app",
-    messagingSenderId: "814617240250",
-    appId: "1:814617240250:web:da0d871eeda9dc0c5bd883"
-  };
+  authDomain: "TU_DOMINIO",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_BUCKET",
+  messagingSenderId: "TU_ID",
+  appId: "TU_APPID"
+};
+
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
