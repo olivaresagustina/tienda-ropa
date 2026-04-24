757211bf255e0be39ef8ed9ebf2d71143a1427a3
olivaresagustina <olivares.financiera@gmail.com>
2026-04-24 15:27:21 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -12,6 +12,50 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const ropaRef = collection(db, "ropa");
 
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+window.registrar = async () => {
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("password").value;
+  
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, pass);
+      alert("Registrado: " + user.user.email);
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+  
+  window.login = async () => {
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("password").value;
+  
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, pass);
+      alert("Bienvenido: " + user.user.email);
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+  
+  window.logout = async () => {
+    try {
+      await signOut(auth);
+      alert("Sesión cerrada");
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+
+
 // CREATE
 window.guardarRopa = async () => {
 
