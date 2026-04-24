9669c580677d71b79e80cd86a583260dba594323
olivaresagustina <olivares.financiera@gmail.com>
2026-04-24 14:51:04 -0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyB0AUy_O2St0qpDDEwJVmtJKZczF9aXEPw",
@@ -14,3 +17,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
