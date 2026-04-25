f335fb03e43fae6f839369f56e48f79f70b6e087
olivaresagustina <olivares.financiera@gmail.com>
2026-04-24 23:42:21 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -77,48 +77,71 @@ window.logout = async () => {
 
 
 // CREATE
+
+
 window.guardarRopa = async () => {
 
-let prenda = document.getElementById("prenda").value;
-let categoria = document.getElementById("categoria").value;
-let precio = document.getElementById("precio").value;
-let stock = document.getElementById("stock").value;
+    let prenda = document.getElementById("prenda").value;
+    let categoria = document.getElementById("categoria").value;
+    let precio = document.getElementById("precio").value;
+    let stock = document.getElementById("stock").value;
+    
+    let archivo = document.getElementById("imagen").files[0];
+    
+    let urlImagen = "";
+    
+    if (archivo) {
+    
+      const formData = new FormData();
+      formData.append("file", archivo);
+      formData.append("upload_preset", "ropa_preset"); // 👈 tu preset
+    
+      const res = await fetch("https://api.cloudinary.com/v1_1/dbmkorqso/image/upload", {
+        method: "POST",
+        body: formData
+      });
+    
+      const data = await res.json();
+      urlImagen = data.secure_url;
+    }
+    
+    await addDoc(ropaRef,{
+      prenda,
+      categoria,
+      precio,
+      stock,
+      imagen: urlImagen
+    });
+    
+    alert("Producto guardado");
+    
+    mostrarRopa();
+    };
 
-await addDoc(ropaRef,{
-prenda,
-categoria,
-precio,
-stock
-});
 
-alert("Producto guardado");
-
-
-mostrarRopa();
-};
 
 async function mostrarRopa(){
 
     let lista = document.getElementById("listaRopa");
-    lista.innerHTML = "";
+    
+
+    
+    lista.innerHTML += `
+<li>
+  <img src="${ropa.imagen || 'https://via.placeholder.com/250'}" 
+       style="width:100%; height:250px; object-fit:cover;">
+
+  <div class="producto-nombre">${ropa.prenda}</div>
+  <div>${ropa.categoria}</div>
+  <div class="producto-precio">$${ropa.precio}</div>
+
+  <button onclick="eliminarRopa('${docu.id}')">Eliminar</button>
+</li>
+`;
+
+      
+    };
   
-    const datos = await getDocs(ropaRef);
-  
-    datos.forEach((docu)=>{
-  
-      let ropa = docu.data();
-  
-      lista.innerHTML += `
-      <li>
-        <div class="producto-nombre">${ropa.prenda}</div>
-        <div>${ropa.categoria}</div>
-        <div class="producto-precio">$${ropa.precio}</div>
-  
-        <button onclick="eliminarRopa('${docu.id}')">Eliminar</button>
-      </li>
-      `;
-    });
-  }
 
 
 // DELETE
