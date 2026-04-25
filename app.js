800f4876bf837bfa75d95bc4edba6cd137e98584
olivaresagustina <olivares.financiera@gmail.com>
2026-04-24 21:20:15 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -104,26 +104,18 @@ lista.innerHTML = "";
 
 const datos = await getDocs(ropaRef);
 
-datos.forEach((docu)=>{
-
-let ropa = docu.data();
-
 lista.innerHTML += `
 <li>
-${ropa.prenda} - ${ropa.categoria} - $${ropa.precio} - Stock: ${ropa.stock}
+  <div class="producto-nombre">${ropa.prenda}</div>
+  <div>${ropa.categoria}</div>
+  <div class="producto-precio">$${ropa.precio}</div>
 
-<button onclick="eliminarRopa('${docu.id}')">Eliminar</button>
-
-<button onclick="editarRopa(
-'${docu.id}',
-'${ropa.prenda}',
-'${ropa.categoria}',
-'${ropa.precio}',
-'${ropa.stock}'
-)">Editar</button>
-
+  <button onclick="eliminarRopa('${docu.id}')">Eliminar</button>
 </li>
 `;
+
+
+
 
 });
 
